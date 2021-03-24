1ed3fbd59bf0be7908201d373e1ba267db4904b2
Diarmuid Murphy <diarmuid.com@gmail.com>
2021-03-24 01:01:31 -0700

    feat: form validation added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,14 +1,35 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 const ContactForm = () => {
 	const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+	const [errorMessage, setErrorMessage] = useState('');
 	const { name, email, message } = formState;
 
 	const handleChange = (e) => {
+		if (e.target.name === 'email') {
+			const isValid = validateEmail(e.target.value);
+			console.log(isValid);
+
+			if (!isValid) {
+				setErrorMessage('Your email is invalid');
+			} else {
+				setErrorMessage('');
+			}
+		} else {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage('');
+			}
+		}
+
 		setFormState({
 			...formState, 
 			[e.target.name]: e.target.value
 		});
+
+		console.log('errorMessage', errorMessage);
 	};
 
 	const handleSubmit = (e) => {
