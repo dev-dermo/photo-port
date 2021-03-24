03f556eff50866052a15185e8c14f1e53a5ecda2
Diarmuid Murphy <diarmuid.com@gmail.com>
2021-03-24 00:50:42 -0700

    feat: form logic done

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Contact = () => {
+const ContactForm = () => {
+	const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+	const { name, email, message } = formState;
+
+	const handleChange = (e) => {
+		setFormState({
+			...formState, 
+			[e.target.name]: e.target.value
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formState);
+	};
+
 	return (
 		<section>
 			<h1>Contact me</h1>
 
-			<form id="contact-form">
+			<form onSubmit={handleSubmit} id="contact-form">
 				<div>
 					<label htmlFor="name">Name:</label>
-					<input type="text" name="name"/>
+					<input
+						type="text"
+						name="name"
+						defaultValue={name}
+						onChange={handleChange}
+					/>
 				</div>
 
 				<div>
 					<label htmlFor="email">Email:</label>
-					<input type="email" name="email"/>
+					<input
+						type="email"
+						name="email"
+						defaultValue={email}
+						onChange={handleChange}
+					/>
 				</div>
 
 				<div>
-					<label htmlFor="message">Name:</label>
-					<textarea name="message" rows="5"/>
+					<label htmlFor="message">Message:</label>
+					<textarea
+						name="message"
+						rows="5"
+						defaultValue={message}
+						onChange={handleChange}
+					/>
 				</div>
 
 				<button type="submit">Submit</button>
@@ -27,4 +57,4 @@ const Contact = () => {
 	);
 };
 
-export default Contact;
+export default ContactForm;
